cfabf00b06cbd66d636248abd160f74715a33175
Alexander Rodin <rodin.alexander@gmail.com>
2019-11-11 18:51:41 +0000

    Can be compiled with all features disabled
    Signed-off-by: Alexander Rodin <rodin.alexander@gmail.com>

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,12 +1,14 @@
 use futures::Future;
-
+#[cfg(feature = "docker")]
 pub mod docker;
+#[cfg(feature = "file")]
 pub mod file;
 pub mod journald;
 #[cfg(feature = "rdkafka")]
 pub mod kafka;
 pub mod statsd;
 pub mod stdin;
+#[cfg(feature = "syslog")]
 pub mod syslog;
 pub mod tcp;
 pub mod udp;
